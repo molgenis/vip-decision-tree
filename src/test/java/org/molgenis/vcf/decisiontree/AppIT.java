add75dd0415875896d4fb3165a6bc442558cba6c
Dennis Hendriksen <d.hendriksen@umcg.nl>
2020-09-11 11:35:52 +0200

    Fix integration test on Travis

--- src/test/java/org/molgenis/vcf/decisiontree/AppIT.java
+++ src/test/java/org/molgenis/vcf/decisiontree/AppIT.java
@@ -35,6 +35,7 @@ class AppIT {
     // output differs every run
     outputVcf = HEADER_VERSION_PATTERN.matcher(outputVcf).replaceAll("##VIP_treeVersion=");
     outputVcf = HEADER_COMMAND_PATTERN.matcher(outputVcf).replaceAll("##VIP_treeCommand=");
+    outputVcf = outputVcf.replaceAll("\\R", "\n");
 
     Path expectedOutputFile = ResourceUtils.getFile("classpath:example-classified.vcf").toPath();
     String expectedOutputVcf = Files.readString(expectedOutputFile);
@@ -57,6 +58,7 @@ class AppIT {
     // output differs every run
     outputVcf = HEADER_VERSION_PATTERN.matcher(outputVcf).replaceAll("##VIP_treeVersion=");
     outputVcf = HEADER_COMMAND_PATTERN.matcher(outputVcf).replaceAll("##VIP_treeCommand=");
+    outputVcf = outputVcf.replaceAll("\\R", "\n");
 
     Path expectedOutputFile = ResourceUtils.getFile("classpath:example-classified_paths-labels.vcf")
         .toPath();
