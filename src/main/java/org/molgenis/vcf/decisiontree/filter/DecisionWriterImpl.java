939e9aa098bd0469ec7d6926c54d45b5fb39cf0c
Dennis Hendriksen <d.hendriksen@umcg.nl>
2020-09-11 14:44:11 +0200

    DecisionWriterImpl unit tests and fixes

--- src/main/java/org/molgenis/vcf/decisiontree/filter/DecisionWriterImpl.java
+++ src/main/java/org/molgenis/vcf/decisiontree/filter/DecisionWriterImpl.java
@@ -8,11 +8,13 @@ import htsjdk.variant.variantcontext.VariantContext;
 import htsjdk.variant.variantcontext.writer.VariantContextWriter;
 import htsjdk.variant.vcf.VCFConstants;
 import java.util.List;
+import java.util.Set;
 import org.molgenis.vcf.decisiontree.filter.model.Decision;
 import org.molgenis.vcf.decisiontree.filter.model.Label;
 import org.molgenis.vcf.decisiontree.filter.model.Node;
 
 public class DecisionWriterImpl implements DecisionWriter {
+
   static final String INFO_CLASS_ID = "VIPC";
   static final String INFO_PATH_ID = "VIPP";
   static final String INFO_LABELS_ID = "VIPL";
@@ -20,6 +22,13 @@ public class DecisionWriterImpl implements DecisionWriter {
   private final VariantContextWriter vcfWriter;
   private final boolean writeLabels;
   private final boolean writePaths;
+
+  /**
+   * Constructs a DecisionWriter that doesn't store labels and paths.
+   */
+  DecisionWriterImpl(VariantContextWriter vcfWriter) {
+    this(vcfWriter, false, false);
+  }
 
   DecisionWriterImpl(VariantContextWriter vcfWriter, boolean writeLabels, boolean writePaths) {
     this.vcfWriter = requireNonNull(vcfWriter);
@@ -38,7 +47,7 @@ public class DecisionWriterImpl implements DecisionWriter {
 
     addDecisionClass(decisions, commonInfo);
     if (writePaths) {
-      addDecisionPath(decisions, commonInfo);
+      addDecisionsPath(decisions, commonInfo);
     }
 
     if (writeLabels) {
@@ -50,34 +59,44 @@ public class DecisionWriterImpl implements DecisionWriter {
   private static void addDecisionLabels(List<Decision> decisions, CommonInfo commonInfo) {
     String infoLabelsValue;
     if (decisions.size() == 1) {
-      infoLabelsValue =
-          decisions.get(0).getLabels().stream().map(Label::getId).collect(joining(","));
+      infoLabelsValue = getVcfLabel(decisions.get(0));
     } else {
       infoLabelsValue =
-          decisions.stream()
-              .map(Decision::getLabels)
-              .map(
-                  labels ->
-                      labels.isEmpty()
-                          ? VCFConstants.MISSING_VALUE_v4
-                          : labels.stream().map(Label::getId).collect(joining("|")))
-              .collect(joining(","));
+          decisions.stream().map(DecisionWriterImpl::getVcfLabel).collect(joining(","));
     }
     commonInfo.putAttribute(INFO_LABELS_ID, infoLabelsValue);
   }
 
-  private static void addDecisionPath(List<Decision> decisions, CommonInfo commonInfo) {
+  private static String getVcfLabel(Decision decision) {
+    String decisionLabel;
+    Set<Label> labels = decision.getLabels();
+    if (!labels.isEmpty()) {
+      decisionLabel = labels.stream().map(Label::getId).collect(joining("|"));
+    } else {
+      decisionLabel = VCFConstants.MISSING_VALUE_v4;
+    }
+    return decisionLabel;
+  }
+
+  private static void addDecisionsPath(List<Decision> decisions, CommonInfo commonInfo) {
     String infoPathValue;
     if (decisions.size() == 1) {
-      infoPathValue = decisions.get(0).getPath().stream().map(Node::getId).collect(joining(","));
+      infoPathValue = getVcfPath(decisions.get(0));
     } else {
-      infoPathValue =
-          decisions.stream()
-              .map(Decision::getPath)
-              .map(nodes -> nodes.stream().map(Node::getId).collect(joining("|")))
-              .collect(joining(","));
+      infoPathValue = decisions.stream().map(DecisionWriterImpl::getVcfPath).collect(joining(","));
     }
     commonInfo.putAttribute(INFO_PATH_ID, infoPathValue);
+  }
+
+  private static String getVcfPath(Decision decision) {
+    String decisionPath;
+    List<Node> path = decision.getPath();
+    if (!path.isEmpty()) {
+      decisionPath = path.stream().map(Node::getId).collect(joining("|"));
+    } else {
+      decisionPath = VCFConstants.MISSING_VALUE_v4;
+    }
+    return decisionPath;
   }
 
   private static void addDecisionClass(List<Decision> decisions, CommonInfo commonInfo) {
