dd9d52173f3e390664224455925d14a92cce6374
bartcharbon <bart.charbon@gmail.com>
2022-07-04 15:14:28 +0200

    remove unused import

--- src/main/java/org/molgenis/vcf/decisiontree/filter/SampleClassifierImpl.java
+++ src/main/java/org/molgenis/vcf/decisiontree/filter/SampleClassifierImpl.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 import org.molgenis.vcf.decisiontree.filter.model.Decision;
 import org.molgenis.vcf.decisiontree.filter.model.DecisionTree;
 import org.molgenis.vcf.decisiontree.runner.VepHelper;
