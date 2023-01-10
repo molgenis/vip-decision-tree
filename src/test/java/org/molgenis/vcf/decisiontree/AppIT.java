0301379262c83ee28168b53aeee052d72f2d89ce
JonathanKlimp <78917640+JonathanKlimp@users.noreply.github.com>
2023-01-10 14:16:36 +0100

    Removed unused test resources and tests from AppIT. Changed the example.vcf and example-classified.vcf to comply with the new expected input and output.

--- src/test/java/org/molgenis/vcf/decisiontree/AppIT.java
+++ src/test/java/org/molgenis/vcf/decisiontree/AppIT.java
@@ -24,10 +24,9 @@ class AppIT {
   @Test
   void test() throws IOException {
     String inputFile = ResourceUtils.getFile("classpath:example.vcf").toString();
-    String treeConfigFile = ResourceUtils.getFile("classpath:example.json").toString();
     String outputFile = sharedTempDir.resolve("example-classified.vcf").toString();
 
-    String[] args = {"-i", inputFile, "-c", treeConfigFile, "-o", outputFile};
+    String[] args = {"-i", inputFile, "-o", outputFile};
     SpringApplication.run(App.class, args);
 
     String outputVcf = Files.readString(Path.of(outputFile));
@@ -37,103 +36,6 @@ class AppIT {
     outputVcf = HEADER_COMMAND_PATTERN.matcher(outputVcf).replaceAll("##VIP_treeCommand=");
 
     Path expectedOutputFile = ResourceUtils.getFile("classpath:example-classified.vcf").toPath();
-    String expectedOutputVcf = Files.readString(expectedOutputFile).replaceAll("\\R", "\n");
-
-    assertEquals(expectedOutputVcf, outputVcf);
-  }
-
-  @Test
-  void testIncludePathsAndLabels() throws IOException {
-    String inputFile = ResourceUtils.getFile("classpath:example.vcf").toString();
-    String treeConfigFile = ResourceUtils.getFile("classpath:example.json").toString();
-    String outputFile = sharedTempDir.resolve("example-classified_paths-labels.vcf").toString();
-
-    String[] args = {"-i", inputFile, "-c", treeConfigFile, "-o", outputFile, "-l", "-p"};
-    SpringApplication.run(App.class, args);
-
-    String outputVcf =
-        Files.readString(Path.of(outputFile));
-
-    // output differs every run due (different tmp dir)
-    outputVcf = HEADER_VERSION_PATTERN.matcher(outputVcf).replaceAll("##VIP_treeVersion=");
-    outputVcf = HEADER_COMMAND_PATTERN.matcher(outputVcf).replaceAll("##VIP_treeCommand=");
-
-    Path expectedOutputFile = ResourceUtils.getFile("classpath:example-classified_paths-labels.vcf")
-        .toPath();
-    String expectedOutputVcf = Files.readString(expectedOutputFile).replaceAll("\\R", "\n");
-    assertEquals(expectedOutputVcf, outputVcf);
-  }
-
-
-  @Test
-  void testSamples() throws IOException {
-    String inputFile = ResourceUtils.getFile("classpath:example_samples.vcf").toString();
-    String treeConfigFile = ResourceUtils.getFile("classpath:example_sample.json").toString();
-    String pedFile = ResourceUtils.getFile("classpath:example_samples.ped").toString();
-    String outputFile = sharedTempDir.resolve("example-classified.vcf").toString();
-
-    String[] args = {"-i", inputFile, "-c", treeConfigFile, "-o", outputFile, "-pb", "Patient",
-        "-m", "samPlE", "-ph", "HP:0000951;HP:0003124", "-pd", pedFile
-    };
-    SpringApplication.run(App.class, args);
-
-    String outputVcf = Files.readString(Path.of(outputFile));
-
-    // output differs every run (different tmp dir)
-    outputVcf = HEADER_VERSION_PATTERN.matcher(outputVcf).replaceAll("##VIP_treeVersion=");
-    outputVcf = HEADER_COMMAND_PATTERN.matcher(outputVcf).replaceAll("##VIP_treeCommand=");
-
-    Path expectedOutputFile = ResourceUtils.getFile("classpath:example_samples-classified.vcf")
-        .toPath();
-    String expectedOutputVcf = Files.readString(expectedOutputFile).replaceAll("\\R", "\n");
-
-    assertEquals(expectedOutputVcf, outputVcf);
-  }
-
-  @Test
-  void testSamplesPathLabels() throws IOException {
-    String inputFile = ResourceUtils.getFile("classpath:example_samples.vcf").toString();
-    String treeConfigFile = ResourceUtils.getFile("classpath:example_sample.json").toString();
-    String outputFile = sharedTempDir.resolve("example-classified.vcf").toString();
-
-    String[] args = {"-i", inputFile, "-c", treeConfigFile, "-o", outputFile, "-pb", "Patient",
-        "-l", "-p", "-m", "sAMPlE", "-ph", "HP:0000951;HP:0003124",};
-    SpringApplication.run(App.class, args);
-
-    String outputVcf = Files.readString(Path.of(outputFile));
-
-    // output differs every run (different tmp dir)
-    outputVcf = HEADER_VERSION_PATTERN.matcher(outputVcf).replaceAll("##VIP_treeVersion=");
-    outputVcf = HEADER_COMMAND_PATTERN.matcher(outputVcf).replaceAll("##VIP_treeCommand=");
-
-    Path expectedOutputFile = ResourceUtils.getFile(
-            "classpath:example_samples-classified_paths-labels.vcf")
-        .toPath();
-    String expectedOutputVcf = Files.readString(expectedOutputFile).replaceAll("\\R", "\n");
-
-    assertEquals(expectedOutputVcf, outputVcf);
-  }
-
-  @Test
-  void testSamplesNoProbandPhenoPed() throws IOException {
-    String inputFile = ResourceUtils.getFile("classpath:example_samples.vcf").toString();
-    String treeConfigFile = ResourceUtils.getFile("classpath:example_sample.json").toString();
-    String outputFile = sharedTempDir.resolve("example-classified-noPPP.vcf").toString();
-
-    String[] args = {"-i", inputFile, "-c", treeConfigFile, "-o", outputFile,
-        "-m", "samPlE", "-l", "-p",
-    };
-    SpringApplication.run(App.class, args);
-
-    String outputVcf = Files.readString(Path.of(outputFile));
-
-    // output differs every run (different tmp dir)
-    outputVcf = HEADER_VERSION_PATTERN.matcher(outputVcf).replaceAll("##VIP_treeVersion=");
-    outputVcf = HEADER_COMMAND_PATTERN.matcher(outputVcf).replaceAll("##VIP_treeCommand=");
-
-    Path expectedOutputFile = ResourceUtils.getFile(
-            "classpath:example_samples-classified-noPPP.vcf")
-        .toPath();
     String expectedOutputVcf = Files.readString(expectedOutputFile).replaceAll("\\R", "\n");
 
     assertEquals(expectedOutputVcf, outputVcf);
