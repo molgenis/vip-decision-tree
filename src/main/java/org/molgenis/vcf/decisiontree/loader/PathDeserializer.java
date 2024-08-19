44a0edadd6980b531eb530cce209d494e8e175c2
dennishendriksen <d.hendriksen@umcg.nl>
2024-08-19 13:53:31 +0200

    Relative file paths are relative to tree file directory

--- src/main/java/org/molgenis/vcf/decisiontree/loader/PathDeserializer.java
+++ src/main/java/org/molgenis/vcf/decisiontree/loader/PathDeserializer.java
@@ -8,12 +8,24 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.IOException;
 import java.nio.file.Path;
 
+import static java.util.Objects.requireNonNull;
+
 public class PathDeserializer extends JsonDeserializer<Path> {
+  private final Path rootPath;
+
+  public PathDeserializer(Path decisionTreeConfigPath) {
+    this.rootPath = requireNonNull(decisionTreeConfigPath);
+  }
+
   @Override
   public Path deserialize(JsonParser jp, DeserializationContext context) throws IOException {
     ObjectCodec objectCodec = jp.getCodec();
     ObjectNode root = objectCodec.readTree(jp);
     String pathString = root.get("path").asText();
-    return Path.of(pathString);
+    Path path = Path.of(pathString);
+    if (!path.isAbsolute()) {
+      path = Path.of(rootPath.getParent().toString(), pathString).normalize();
+    }
+    return path;
   }
 }
